e357bb02de68bc6b55ce6f0deef20bc37a337ce7
SVolochandrey <113455948+SVolochandrey@users.noreply.github.com>
2023-05-04 10:08:20 +0000

    Deploying to gh-pages from @ SVolochandrey/goit-react-hw-05-movies@a9c3c70aae54d8be4660b9c27d899f14c7373824 🚀

---
+++ static/js/805.c7b9f1d0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{9805:function(e,t,n){n.r(t);var r=n(1087),o=n(184);t.default=function(){return(0,o.jsxs)(o.Fragment,{children:[(0,o.jsx)("h2",{children:"Oops... nothing found..."}),(0,o.jsx)(r.rU,{to:"/",children:"Back to home"})]})}}}]);
+//# sourceMappingURL=805.c7b9f1d0.chunk.js.map
